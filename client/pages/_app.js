3ed5e85388239d8cd3df1eb08eec6167b2046796
Ashwin S <aswinjeganath@gmail.com>
2018-12-02 21:48:17 +0530

    Add page transitions, fix header styling

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -1,31 +1,54 @@
-import App, { Container } from 'next/app';
-import { ApolloProvider } from 'react-apollo';
-import withData from '../lib/withData';
-import Page from '../components/Page';
+import App, { Container } from "next/app";
+import { ApolloProvider } from "react-apollo";
+import { PageTransition } from "next-page-transitions";
+import { ThemeProvider } from "styled-components";
+
+import withData from "../lib/withData";
+import Page from "../components/Page";
+import Header from "../components/Header";
 
 class MyApp extends App {
-	static async getInitialProps({ Component, ctx }) {
-		let pageProps = {};
-		if (Component.getInitialProps) {
-			pageProps = await Component.getInitialProps(ctx);
-		}
-		// this exposes the query to the user
-		pageProps.query = ctx.query;
-		return { pageProps };
-	}
-	render() {
-		const { Component, apollo, pageProps } = this.props;
+  static async getInitialProps({ Component, ctx }) {
+    let pageProps = {};
+    if (Component.getInitialProps) {
+      pageProps = await Component.getInitialProps(ctx);
+    }
+    // this exposes the query to the user
+    pageProps.query = ctx.query;
+    return { pageProps };
+  }
+  render() {
+    const { Component, apollo, pageProps } = this.props;
 
-		return (
-			<Container>
-				<ApolloProvider client={apollo}>
-					<Page>
-						<Component {...pageProps} />
-					</Page>
-				</ApolloProvider>
-			</Container>
-		);
-	}
+    return (
+      <Container>
+        <Header />
+        <PageTransition timeout={300} classNames="page-transition">
+          <ApolloProvider client={apollo}>
+            <Page>
+              <Component {...pageProps} />
+            </Page>
+          </ApolloProvider>
+        </PageTransition>
+        <style jsx global>{`
+          .page-transition-enter {
+            opacity: 0;
+          }
+          .page-transition-enter-active {
+            opacity: 1;
+            transition: opacity 300ms;
+          }
+          .page-transition-exit {
+            opacity: 1;
+          }
+          .page-transition-exit-active {
+            opacity: 0;
+            transition: opacity 300ms;
+          }
+        `}</style>
+      </Container>
+    );
+  }
 }
 
 export default withData(MyApp);
