4be5cbfb22db4c7f15a733140b2828c6a968e027
Lauren <llwor94@gmail.com>
2018-12-05 21:19:22 -0800

    user search displays users

--- client/components/FriendsSearch.js
+++ client/components/FriendsSearch.js
@@ -12,9 +12,17 @@ class FriendsSearch extends Component {
 
 	onChange = debounce(async (e, client) => {
 		console.log(e.target);
+		this.setState({ loading: true });
 		const res = await client.query({
 			query: SEARCH_USERS_QUERY,
 			variables: { searchTerm: e.target.value },
+		});
+
+		let searchField = res.data.allUsers.length ? res.data.allUsers : [];
+
+		this.setState({
+			items: searchField,
+			loading: false,
 		});
 
 		console.log(res.data);
@@ -32,21 +40,42 @@ class FriendsSearch extends Component {
 						)}
 					itemToString={item => (item === null ? '' : item.name)}
 				>
-					{({ getInputProps, getItemProps, isOpen, inputValue, highlightedIndex }) => (
+					{({
+						getInputProps,
+						getMenuProps,
+						getItemProps,
+						isOpen,
+						inputValue,
+						highlightedIndex,
+					}) => (
 						<div>
-							<ApolloConsumer>
-								{client => (
-									<input
-										{...getInputProps({
-											placeholder: 'Search...',
-											onChange: e => {
-												e.persist();
-												this.onChange(e, client);
-											},
-										})}
-									/>
-								)}
-							</ApolloConsumer>
+							<div>
+								<ApolloConsumer>
+									{client => {
+										console.log(client);
+										return (
+											<input
+												{...getInputProps({
+													placeholder: 'Search...',
+													onChange: e => {
+														e.persist();
+														this.onChange(e, client);
+													},
+												})}
+											/>
+										);
+									}}
+								</ApolloConsumer>
+							</div>
+							<div>
+								<ul>
+									{isOpen ? (
+										this.state.items.map((item, i) => (
+											<li key={item.id}>{item.username}</li>
+										))
+									) : null}
+								</ul>
+							</div>
 						</div>
 					)}
 				</Downshift>
